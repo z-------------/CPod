ed2428bb2539f2ab895ba02915b6a0099c69ce5f
Zack Guard <zacharyguard@gmail.com>
2015-06-07 18:04:51 +0800

    Update feeds (incomplete)

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-var storage = chrome.storage.sync;
+var storage = chrome.storage;
 
 var xhr = function(url, callback) {
     var oReq = new XMLHttpRequest();
@@ -11,22 +11,43 @@ var xhr = function(url, callback) {
 
 var ALARM_NAME = "feed_updater";
 
+var feedContents;
+
 chrome.alarms.create(ALARM_NAME, {
-    periodInMinutes: 6/60
+    periodInMinutes: 30/60
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     console.log("onAlarm fired on " + alarm.name);
     if (alarm.name === ALARM_NAME) {
+        feedContents = {};
+
         console.log("starting feeds update");
-        storage.get("feeds", function(r) {
+        storage.sync.get("feeds", function(r) {
             var feeds = r.feeds || [];
             feeds.forEach(function(feed) {
                 console.log("starting update of feed '" + feed.title +  "'");
                 xhr(feed.url, function(res) {
-                    console.log("xhr'd feed '" + feed.title +  "'\n", res);
+                    var xmlDoc = $.parseXML(res);
+                    var $xml = $(xmlDoc);
+
+                    var items = $xml.find("item");
+                    console.log(items);
+
+                    feedContents[feed.url] = { items: [] };
+                    var feedContent = feedContents[feed.url];
+
+                    [].slice.call(items).forEach(function(elem) {
+                        feedContent.items.push({
+                            title: elem.querySelector("title").textContent,
+                            date: new Date(elem.querySelector("pubDate").textContent) || null,
+                            url: elem.querySelector("enclosure[url]").getAttribute("url")
+                        });
+                    });
+
+                    console.log("done updating feed '" + feed.title +  "'");
                 });
-            })
+            });
         });
     }
 });
