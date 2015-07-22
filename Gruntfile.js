cd3e4b4411810f5c52605fdae3d4318bce84eb4b
Zachary Guard <zacharyguard@gmail.com>
2015-07-22 20:56:15 +0800

    More grunt automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,45 @@
 module.exports = function(grunt) {
-
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         concat: {
             options: {
                 separator: ";\n",
                 process: function(src, path) {
-                    return "/* " + path + " */\n" + src;
+                    return "\n/* " + path + " */\n\n" + src;
                 },
             },
-            dist: {
-                src: ["src/requires.js", "src/routing.js", "src/feedinfo.js", "src/update.js", "src/server_start.js"],
-                dest: "server.js",
+            server: {
+                src: [
+                    "src/server/requires.js",
+                    "src/server/routing.js",
+                    "src/server/feedinfo.js",
+                    "src/server/update.js",
+                    "src/server/server_start.js"
+                ],
+                dest: "dist/server.js",
             },
+            client: {
+                src: [
+                    "src/public/app/main.css",
+                    "src/public/app/material-styles.css"
+                ],
+                dest: "dist/public/app/style.css"
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                cwd: "lib/",
+                src: "**",
+                dest: "dist/public/assets/"
+            }
         }
     });
 
     // Load plugins
     grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
     // Register default tasks
-    grunt.registerTask("default", ["concat"]);
+    grunt.registerTask("default", ["concat", "copy"]);
 };
