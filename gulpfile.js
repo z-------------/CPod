027e62ee3f0c6fa1dd065639bbe6bba5422d7d99
Zack Guard <zacharyguard@gmail.com>
2015-09-06 20:28:43 +0800

    SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require("gulp");
 
-gulp.task("autoprefixer", function () {
+gulp.task("css", function() {
     var postcss = require("gulp-postcss");
     var autoprefixer = require("autoprefixer-core");
     var concat = require("gulp-concat");
+    var sass = require("gulp-sass");
 
-    return gulp.src("./public/app/style.unprefixed.css")
+    return gulp.src("./public/app/style.unprefixed.scss")
+        .pipe(sass().on("error", sass.logError))
         .pipe(postcss([ autoprefixer({ browsers: ["last 2 versions"] }) ]))
         .pipe(concat("style.css"))
         .pipe(gulp.dest("./public/app/"));
+});
+
+gulp.task("css:watch", function() {
+    gulp.watch("./public/app/style.unprefixed.scss", ["css"]);
 });
