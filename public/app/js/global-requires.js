e0e9e6c4dc82d1b3ea065d3852e3157edeed8712
Zachary Guard <zacharyguard@gmail.com>
2017-09-10 17:28:38 +0800

    Load audiosearch creds from env vars, not a file
    This is what was making all the CI-built Cumulonimbi not work -- the file that I used to use for storing these creds is gitignored (for obvious reasons)

--- public/app/js/global-requires.js
+++ public/app/js/global-requires.js
@@ -5,6 +5,6 @@ const path = require("path")
 
 const Audiosearch = require("audiosearch-client-node")
 const audiosearch = new Audiosearch(
-  require("../../audiosearch_config.json").application_id,
-  require("../../audiosearch_config.json").secret
+  process.env.CBUS_AUDIOSEARCH_APPLICATION_ID,
+  process.env.CBUS_AUDIOSEARCH_SECRET
 )
