3490f24798ffb7bfdef2aed4bce8ba6d303a42ad
Zack Guard <zacharyguard@gmail.com>
2018-03-14 22:28:34 +0800

    Favor <image> over <itunes:image> when getting podcast image url

--- public/app/js/cbus-server-get-podcast-info.js
+++ public/app/js/cbus-server-get-podcast-info.js
@@ -39,10 +39,10 @@ if (!cbus.hasOwnProperty("server")) { cbus.server = {} }
           let imageElems = doc.querySelectorAll("rss channel > image");
           for (let i = 0, l = imageElems.length; i < l; i++) {
             let imageElem = imageElems[i];
-            if (imageElem.tagName === "itunes:image") {
-              podcastData.image = imageElem.getAttribute("href");
-            } else if (imageElem.tagName === "image" && imageElem.getElementsByTagName("url")[0]) {
+            if (imageElem.tagName === "image" && imageElem.getElementsByTagName("url")[0]) {
               podcastData.image = imageElem.getElementsByTagName("url")[0].textContent;
+            } else if (imageElem.tagName === "itunes:image") {
+              podcastData.image = imageElem.getAttribute("href");
             }
           }
 
