123aaa244f0a6f5aff9f9be798562416c0963d94
Zack Guard <zacharyguard@gmail.com>
2018-03-16 12:20:19 +0800

    Used cached popular podcasts for 1 day

--- public/app/js/cbus-server-get-popular-podcasts.js
+++ public/app/js/cbus-server-get-popular-podcasts.js
@@ -3,28 +3,40 @@ if (!cbus.hasOwnProperty("server")) { cbus.server = {} }
 (function() {
   cbus.server.getPopularPodcasts = function(callback) {
     console.log("getPopularPodcasts")
-    xhr("https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/10/explicit.json", function(err, status, r) {
-      let result = []
+    localforageGetMulti(["cbus_popular_podcasts_cache", "cbus_popular_podcasts_cache_time"], (r) => {
+      if (
+        r.cbus_popular_podcasts_cache &&
+        r.cbus_popular_podcasts_cache_time &&
+        new Date() - r.cbus_popular_podcasts_cache_time < 86400000 // 1 day
+      ) {
+        callback(r.cbus_popular_podcasts_cache)
+      } else {
+        xhr("https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/10/explicit.json", function(err, status, r) {
+          let result = []
 
-      let popularPodcasts = JSON.parse(r).feed.results
-      var doneCount = 0
+          let popularPodcasts = JSON.parse(r).feed.results
+          var doneCount = 0
 
-      for (let i = 0, l = popularPodcasts.length; i < l; i++) {
-        xhr("https://itunes.apple.com/lookup?id=" + popularPodcasts[i].id, function(err, status, r) {
-          let showInfo = JSON.parse(r).results[0]
-          cbus.server.getPodcastInfo(showInfo.feedUrl, function(podcastInfo) {
-            doneCount += 1
-            if (podcastInfo !== null) {
-              podcastInfo.url = showInfo.feedUrl
-              result.push(podcastInfo)
-              console.log(`getPopularPodcasts done ${doneCount}/${popularPodcasts.length}`)
-            } else {
-              console.log(`getPopularPodcasts FAIL ${doneCount}/${popularPodcasts.length}`)
-            }
-            if (doneCount === popularPodcasts.length) {
-              callback(result)
-            }
-          })
+          for (let i = 0, l = popularPodcasts.length; i < l; i++) {
+            xhr("https://itunes.apple.com/lookup?id=" + popularPodcasts[i].id, function(err, status, r) {
+              let showInfo = JSON.parse(r).results[0]
+              cbus.server.getPodcastInfo(showInfo.feedUrl, function(podcastInfo) {
+                doneCount += 1
+                if (podcastInfo !== null) {
+                  podcastInfo.url = showInfo.feedUrl
+                  result.push(podcastInfo)
+                  console.log(`getPopularPodcasts done ${doneCount}/${popularPodcasts.length}`)
+                } else {
+                  console.log(`getPopularPodcasts FAIL ${doneCount}/${popularPodcasts.length}`)
+                }
+                if (doneCount === popularPodcasts.length) {
+                  callback(result)
+                  localforage.setItem("cbus_popular_podcasts_cache", result)
+                  localforage.setItem("cbus_popular_podcasts_cache_time", new Date().getTime())
+                }
+              })
+            })
+          }
         })
       }
     })
