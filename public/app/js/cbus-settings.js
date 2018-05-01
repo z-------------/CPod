0efa02b3f72d76d2ed61a8265020ff2058f1c3a4
Zack Guard <zacharyguard@gmail.com>
2018-05-01 12:13:47 +0800

    Gracefully handle invalid settings json

--- public/app/js/cbus-settings.js
+++ public/app/js/cbus-settings.js
@@ -20,19 +20,16 @@ cbus.settings = {
 };
 
 (function() {
-  var userSettingsFile;
   try {
     userSettingsFile = fs.readFileSync(cbus.settings.SETTINGS_FILE_PATH, {
       encoding: "utf8"
     });
-  } catch (e) {
-    console.log("no user settings file");
-  }
-  if (userSettingsFile) {
     let userSettings = JSON.parse(userSettingsFile);
     for (let key in userSettings) {
       cbus.settings.data[key] = userSettings[key];
     }
+  } catch (e) {
+    console.log("no user settings file");
   }
 }());
 
