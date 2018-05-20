116e331634dae2b616ac5466efd92d46e093de5e
Zack Guard <zacharyguard@gmail.com>
2018-05-20 18:35:06 +0800

    4-space to 2-space in basic.js

--- public/app/js/basic.js
+++ public/app/js/basic.js
@@ -54,58 +54,58 @@ const xhr = function(options, callback) {
 };
 
 const colonSeparateDuration = function(num) { // in seconds
-    if (typeof num == "number" && !(Number.isNaN || isNaN)(num)) {
-        var hours = Math.floor(num / 60 / 60);
-        var minutes = Math.floor(num / 60) - hours * 60;
-        var seconds = Math.floor(num % 60);
-        return (hours !== 0 ? "" + hours + ":" : "") + zpad(minutes, 2) + ":" + zpad(seconds, 2);
-    } else {
-        return "--:--";
-    }
+  if (typeof num == "number" && !(Number.isNaN || isNaN)(num)) {
+    var hours = Math.floor(num / 60 / 60);
+    var minutes = Math.floor(num / 60) - hours * 60;
+    var seconds = Math.floor(num % 60);
+    return (hours !== 0 ? "" + hours + ":" : "") + zpad(minutes, 2) + ":" + zpad(seconds, 2);
+  } else {
+    return "--:--";
+  }
 };
 
 const zpad = function pad(n, width, z) { // by user Pointy on SO: stackoverflow.com/a/10073788
-    z = z || "0";
-    n = n + "";
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  z = z || "0";
+  n = n + "";
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 };
 
 const mergeObjects = function(a, b){
-    var result = {};
+  var result = {};
 
-    for (var key in a) {
-        result[key] = a[key];
-    }
-    for (var key in b) {
-        result[key] = b[key];
-    }
+  for (var key in a) {
+    result[key] = a[key];
+  }
+  for (var key in b) {
+    result[key] = b[key];
+  }
 
-    return result;
+  return result;
 };
 
 const arrayFindByKey = function(arr, key, val) {
-    let results = [];
+  let results = [];
 
-    for (let i = 0, l = arr.length; i < l; i++) {
-        if (arr[i][key] === val) {
-            results.push(arr[i]);
-        }
+  for (let i = 0, l = arr.length; i < l; i++) {
+    if (arr[i][key] === val) {
+      results.push(arr[i]);
     }
+  }
 
-    return results;
+  return results;
 };
 
 const arrayFindByKeySingle = function(arr, key, val) {
-    var result;
+  var result;
 
-    for (let i = 0, l = arr.length; i < l; i++) {
-        if (arr[i][key] === val) {
-            result = arr[i];
-            break;
-        }
+  for (let i = 0, l = arr.length; i < l; i++) {
+    if (arr[i][key] === val) {
+      result = arr[i];
+      break;
     }
+  }
 
-    return result;
+  return result;
 };
 
 const htmlTagsRegex = /(<([^>]+)>)/gi; // https://css-tricks.com/snippets/javascript/strip-html-tags-in-javascript/
@@ -118,73 +118,73 @@ const decodeHTML = function(html) {
 };
 
 const removeHTMLTags = function(html) {
-    return html.replace(/(<([^>]+)>)/ig, ""); // css-tricks.com/snippets/javascript/strip-html-tags-in-javascript
+  return html.replace(/(<([^>]+)>)/ig, ""); // css-tricks.com/snippets/javascript/strip-html-tags-in-javascript
 };
 
 const KEYCODES = {
-    "0": 48,
-    "1": 49,
-    "2": 50,
-    "3": 51,
-    "4": 52,
-    "5": 53,
-    "6": 54,
-    "7": 55,
-    "8": 56,
-    "9": 57,
-    "A": 65,
-    "B": 66,
-    "C": 67,
-    "D": 68,
-    "E": 69,
-    "F": 70,
-    "G": 71,
-    "H": 72,
-    "I": 73,
-    "J": 74,
-    "K": 75,
-    "L": 76,
-    "M": 77,
-    "N": 78,
-    "O": 79,
-    "P": 80,
-    "Q": 81,
-    "R": 82,
-    "S": 83,
-    "T": 84,
-    "U": 85,
-    "V": 86,
-    "W": 87,
-    "X": 88,
-    "Y": 89,
-    "Z": 90,
-    "a": 97,
-    "b": 98,
-    "c": 99,
-    "d": 100,
-    "e": 101,
-    "f": 102,
-    "g": 103,
-    "h": 104,
-    "i": 105,
-    "j": 106,
-    "k": 107,
-    "l": 108,
-    "m": 109,
-    "n": 110,
-    "o": 111,
-    "p": 112,
-    "q": 113,
-    "r": 114,
-    "s": 115,
-    "t": 116,
-    "u": 117,
-    "v": 118,
-    "w": 119,
-    "x": 120,
-    "y": 121,
-    "z": 122,
-    "_space": 32
+  "0": 48,
+  "1": 49,
+  "2": 50,
+  "3": 51,
+  "4": 52,
+  "5": 53,
+  "6": 54,
+  "7": 55,
+  "8": 56,
+  "9": 57,
+  "A": 65,
+  "B": 66,
+  "C": 67,
+  "D": 68,
+  "E": 69,
+  "F": 70,
+  "G": 71,
+  "H": 72,
+  "I": 73,
+  "J": 74,
+  "K": 75,
+  "L": 76,
+  "M": 77,
+  "N": 78,
+  "O": 79,
+  "P": 80,
+  "Q": 81,
+  "R": 82,
+  "S": 83,
+  "T": 84,
+  "U": 85,
+  "V": 86,
+  "W": 87,
+  "X": 88,
+  "Y": 89,
+  "Z": 90,
+  "a": 97,
+  "b": 98,
+  "c": 99,
+  "d": 100,
+  "e": 101,
+  "f": 102,
+  "g": 103,
+  "h": 104,
+  "i": 105,
+  "j": 106,
+  "k": 107,
+  "l": 108,
+  "m": 109,
+  "n": 110,
+  "o": 111,
+  "p": 112,
+  "q": 113,
+  "r": 114,
+  "s": 115,
+  "t": 116,
+  "u": 117,
+  "v": 118,
+  "w": 119,
+  "x": 120,
+  "y": 121,
+  "z": 122,
+  "_space": 32
 };
 
 const localforageGetMulti = function(keys, callback) {
