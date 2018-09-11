08fb8f04e2dacc6046188b98399edad4e91b630a
Zack Guard <zacharyguard@gmail.com>
2018-09-11 18:16:41 +0800

    Allow img tag in episode descriptions

--- public/app/js/global-requires.js
+++ public/app/js/global-requires.js
@@ -32,7 +32,7 @@ const sanitizeHTML = require("sanitize-html")
 sanitizeHTML.defaults.allowedTags = [
   "h1", "h2", "h3", "h4", "h5", "h6", "blockquote", "p", "a", "ul", "ol", "nl",
   "li", "b", "i", "strong", "em", "strike", "code", "hr", "br", "div",  "table",
-  "thead", "caption", "tbody", "tr", "th", "td", "pre"
+  "thead", "caption", "tbody", "tr", "th", "td", "pre", "img"
 ]
 
 var MPRISPlayer;
