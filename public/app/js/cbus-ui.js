6e1cc931384f8c18d8f2e1e638bd96e7066aa269
Zack Guard <zacharyguard@gmail.com>
2024-01-12 09:25:12 +0800

    Show items in folder instead of opening

--- public/app/js/cbus-ui.js
+++ public/app/js/cbus-ui.js
@@ -1024,11 +1024,11 @@ $(".settings_button--update-feed-artworks").on("click", function() {
 
 $(".settings_button--manage-downloaded-episodes").on("click", function() {
   let downloadedEpisodesPath = cbus.settings.data.downloadDirectory;
-  remote.shell.openItem(downloadedEpisodesPath);
+  remote.shell.showItemInFolder(downloadedEpisodesPath);
 });
 
 getElem(".settings_button--open-config-dir").addEventListener("click", e => {
-  remote.shell.openItem(cbus.const.USERDATA_PATH);
+  remote.shell.showItemInFolder(cbus.const.USERDATA_PATH);
 });
 
 document.getElementsByClassName("settings_version-string")[0].textContent = package.version;
